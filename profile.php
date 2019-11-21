2ed0ecdebf58e55772d944ef16bfb984d58993e4
cuongdhgch17303 <58015761+cuongdhgch17303@users.noreply.github.com>
2019-11-21 11:14:57 +0700

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Infomation</title>
+    <meta charset="utf-8">
+</head>
+<body>
+   <h2>Infomation of Manh Hung: </h2>
+  <img src="https://www.facebook.com/profile.php?id=100003777143367" alt="facebook.com">
+</body>
+</html>
