8d4db628c5ef3bde5b238f26f1752d29a8cc30c5
cuongdhgch17303 <58015761+cuongdhgch17303@users.noreply.github.com>
2019-11-21 11:35:38 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -7,5 +7,10 @@
 <body>
    <h2>Infomation of Hung Cuong	: </h2>
   <img src="https://media.wired.com/photos/5d72a4682f4c730008bbaad8/191:100/w_2400,h_1256,c_limit/Transpo_RU_P19_0578_a3_rgb.jpg" alt="facebook.com">
+  Name: <input type="text" name="name"><br>
+   <h2>Picture: </h2>
+   <img src="hung.jpg" width="150" height="100"><br>
+  School: <input type="text" name="name"><br>
+   <input type="button" onclick="alert('')" value="OK">
 </body>
 </html>
